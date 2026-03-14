bdf629379d07e9cca728b60dff997d77c8b9b6be
DaniloSVJ <danilocotatoceu@gmail.com>
2026-03-14 00:45:54 -0300

    ajuste

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'http://localhost:5173', // ou um array de domínios ['http://site1.com', 'http://site2.com']
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, // permite cookies
-  });
+  app.enableCors();
   await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
